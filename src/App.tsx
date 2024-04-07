e55288c7beec906eac95e0f0bfc988c50c812a8a
Jakub <jakub.latko@vp.pl>
2024-04-07 17:48:46 +0200

    basic markdown functionality

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,106 @@ function addDefaultTheme() {
 	}
 }
 
+function createPreview() {
+	let fileContent: string[][] = [];
+	const editingArea =
+		document.querySelector<HTMLTextAreaElement>("#editingArea");
+	const previewArea = document.querySelector<HTMLDivElement>("#previewArea");
+	if (!editingArea || !previewArea) return;
+	previewArea.innerHTML = "";
+	const textToEdit = editingArea.value;
+
+	let splittedLines = textToEdit.split("\n");
+
+	splittedLines.forEach((line) => {
+		fileContent.push(line.split(" "));
+	});
+
+	fileContent.forEach((line) => {
+		switch (line[0]) {
+			case "######":
+				const h6 = document.createElement("h6");
+				h6.className = "h6-md";
+				h6.textContent = line.join(" ").substring(6);
+				previewArea.appendChild(h6);
+				break;
+			case "#####":
+				const h5 = document.createElement("h5");
+				h5.className = "h5-md";
+				h5.textContent = line.join(" ").substring(5);
+				previewArea.appendChild(h5);
+				break;
+			case "####":
+				const h4 = document.createElement("h4");
+				h4.className = "h4-md";
+				h4.textContent = line.join(" ").substring(4);
+				previewArea.appendChild(h4);
+				break;
+			case "###":
+				const h3 = document.createElement("h3");
+				h3.className = "h3-md";
+				h3.textContent = line.join(" ").substring(3);
+				previewArea.appendChild(h3);
+				break;
+			case "##":
+				const h2 = document.createElement("h2");
+				h2.className = "h2-md";
+				h2.textContent = line.join(" ").substring(2);
+				previewArea.appendChild(h2);
+				break;
+
+			case "#":
+				const h1 = document.createElement("h2");
+				h1.className = "h1-md";
+				h1.textContent = line.join(" ").substring(1);
+				previewArea.appendChild(h1);
+				break;
+			case ">":
+				const blockquote = document.createElement("blockquote");
+				blockquote.className = "blockquote-md";
+				blockquote.textContent = line.join(" ").substring(1);
+				previewArea.appendChild(blockquote);
+				break;
+			case "-":
+				const unorderedElem = document.createElement("div");
+				unorderedElem.innerHTML = `<span></span><p>${line
+					.join(" ")
+					.substring(1)}</p>`;
+				unorderedElem.className = "unordered-element-md";
+				previewArea.appendChild(unorderedElem);
+				break;
+			case "":
+				const emptyLine = document.createElement("span");
+				emptyLine.className = "empty-line-md";
+				previewArea.appendChild(emptyLine);
+				break;
+			default:
+				switch (true) {
+					case /\d\./.test(line[0]):
+						const orderedElem = document.createElement("div");
+						orderedElem.innerHTML = `<span>${
+							line[0]
+						}</span><p>${line
+							.join(" ")
+							.substring(line[0].length)}</p>`;
+						orderedElem.className = "ordered-element-md";
+
+						previewArea.appendChild(orderedElem);
+
+						break;
+
+					default:
+						const span = document.createElement("span");
+						span.className = "paragraph-md";
+						span.textContent = line.join(" ");
+						previewArea.appendChild(span);
+						break;
+				}
+		}
+	});
+	console.log(fileContent);
+}
+
 function App() {
 	return (
 		<>
@@ -135,11 +235,11 @@ function App() {
 						name="editingArea"
 						id="editingArea"
 						className="edit-md"
-						onChange={() => console.log("zmiana")}></textarea>
+						onChange={() => createPreview()}></textarea>
 				</section>
 				<section className="previewSection">
 					<div className="sectionHeader">
-						<h2 className="heading-small">Preview</h2>
+						<h2 className="heading-small">PREVIEW</h2>
 						<button>
 							<img src="/assets/icon-show-preview.svg" alt="" />
 						</button>
